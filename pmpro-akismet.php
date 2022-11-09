c7d42930f908ed32bb5dc518816d3987515899e4
Andrew Lima <andrew@arctek.co.za>
2022-11-09 15:55:53 +0200

    Improve the admin notice logic.
    * Improve the admin notice logic to check for: PMPro, Akismet and that there is a valid API key in Akismet to use for anti-spam

--- pmpro-akismet.php
+++ pmpro-akismet.php
@@ -10,6 +10,8 @@
  * Domain Path: /languages
  */
 
+use PMPro_Akismet\Akismet;
+
 /**
  * Includes go here.
  */
@@ -22,11 +24,33 @@ require_once( dirname( __FILE__ ) . '/includes/checkout.php' );
  * @since 1.0
  */
 function pmpro_akismet_pmpro_required() {
-    if ( is_plugin_active( 'paid-memberships-pro/paid-memberships-pro.php' ) ) {
+
+    // Only show notices on PMPro page.
+    if ( ! isset( $_REQUEST['page'] ) || strpos( $_REQUEST['page'], 'pmpro' ) === false ) {
         return;
     }
 
-    echo '<div class="error"><p>' . __( 'The Paid Memberships Pro - Akismet Integration add-on requires the Paid Memberships Pro plugin. Please install and activate Paid Memberships Pro before activating this add-on.', 'pmpro-akismet' ) . '</p></div>';
+    // The required plugins for this Add On to work.
+    $required_plugins = array(
+        'paid-memberships-pro/paid-memberships-pro.php' => __( 'Paid Memberships Pro', 'pmpro-akismet' ),
+        'akismet/akismet.php' => __( 'Akismet', 'pmpro-akismet' )
+    );
+
+    foreach( $required_plugins as $file => $plugin_name ) {
+        if ( ! is_plugin_active( $file ) ) {
+            $message = sprintf( __( 'The %s plugin requires the %s plugin to be active.', 'pmpro-akismet' ), 'Paid Memberships Pro - Akismet Integration', $plugin_name );
+            echo '<div class="error"><p>' . esc_html( $message ) . '</p></div>';
+            return;
+        }
+
+    }
+
+    //Check if there's a valid API key in Akismet, if not show a message
+    if ( ! Akismet::has_valid_key() ) {
+        echo '<div class="error"><p>' . __( 'The Paid Memberships Pro - Akismet Integration add-on requires a valid API key in Akismet. Please enter a valid API key in Akismet to enable anti-spam functionality.', 'pmpro-akismet' ) . '</p></div>';
+        return;
+    }
+
 }
 add_action( 'admin_notices', 'pmpro_akismet_pmpro_required' );
 
@@ -39,4 +63,3 @@ function pmpro_akismet_load_textdomain() {
     load_plugin_textdomain( 'pmpro-akismet', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 }
 add_action( 'plugins_loaded', 'pmpro_akismet_load_textdomain' );
-
